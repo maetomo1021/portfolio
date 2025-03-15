fa24a8d13ec4eddd6de6e436f557cdede700d3d0
maetomo1021 <maetomoda@gmail.com>
2025-03-15 17:13:38 +0900

    ログインフォームの実施

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from flask import Flask,render_template
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
 def helloworld():
-    return render_template("index.html")
+    age = 29
+    name = "Taro"
+    return render_template("index.html",age = age,name =name)
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run()
